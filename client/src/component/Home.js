4b4b9761bd9b474bb196ab881d36fc1300ea5a3b
sharmila-developer <sharmi.webdev@gmail.com>
2024-05-29 19:01:07 -0700

    adding more

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
 
         <nav className='nav-bar'>
           <ul>
-            <li><Link to='/' >Home </Link></li>
+            <li><Link to='/home' >Home </Link></li>
             <li><Link >About</Link></li>
             <li><Link >Service </Link></li>
             <li><Link>Contact </Link></li> 
